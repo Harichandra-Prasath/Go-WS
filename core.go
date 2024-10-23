b3b55ed472a752994326af90ebf8f0349ff2980f
Harichandra-Prasath <1hcp302004@gmail.com>
2024-10-23 12:11:13 +0530

    Basic Echo Server

--- core.go
+++ core.go
@@ -1,1 +1,22 @@
 package gows
+
+import (
+	"fmt"
+	"net"
+)
+
+// Handles the Initial Handshake parsing the headers
+func handleHandShake(conn *net.TCPConn) error {
+
+	// Read the Initial Handshake
+	buff := make([]byte, 1024)
+
+	n, err := conn.Read(buff)
+	if err != nil {
+		return fmt.Errorf("reading handshake: %s", err)
+	}
+
+	conn.Write(buff[:n])
+
+	return nil
+}
---
+++ server.go
@@ -0,0 +1,60 @@
+package gows
+
+import (
+	"fmt"
+	"net"
+)
+
+// Configuration of the WebSocket Server
+type ServerConfig struct {
+
+	// Addr used by the Server
+	Addr string
+}
+
+type WebSocketServer struct {
+	config ServerConfig
+}
+
+// Create a New Server with given configuration and return the reference
+func NewWebSocketServer(cfg ServerConfig) *WebSocketServer {
+
+	return &WebSocketServer{
+		config: cfg,
+	}
+
+}
+
+// Starts the Server and Handles the Incoming Connections
+func (S *WebSocketServer) Start() error {
+
+	_listener, err := net.Listen("tcp", S.config.Addr)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Server listening at localhost%s\n", S.config.Addr)
+
+	for {
+		conn, err := _listener.Accept()
+		if err != nil {
+			fmt.Println("Error in getting the connection:", err)
+			continue
+		}
+
+		fmt.Printf("Got a %s connection with addr:%s\n", conn.RemoteAddr().Network(), conn.RemoteAddr().String())
+
+		go S.handleConn(conn.(*net.TCPConn))
+	}
+
+}
+
+// Handles the incoming Connections
+func (S *WebSocketServer) handleConn(conn *net.TCPConn) {
+	err := handleHandShake(conn)
+	if err != nil {
+		fmt.Println("handleHandShake:", err)
+		return
+	}
+}
